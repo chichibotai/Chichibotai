30f771b670b7336d95eb53b5bf3312c945189805
Monica Bouveret <djvodwall@gmail.com>
2024-04-04 21:27:08 +0200

    Initial Commit

---
+++ Chichibotai/ChichibotaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChichibotaiApp.swift
+//  Chichibotai
+//
+//  Created by Monica Bouveret on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ChichibotaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
