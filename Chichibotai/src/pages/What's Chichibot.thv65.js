8718dbf25751b1d99953f1a12822d782d2e4b1c6
Mo Bouveret <166317807+chichibotai@users.noreply.github.com>
2024-09-25 16:37:20 +0200

    Add files via upload

---
+++ Chichibotai/src/pages/What's Chichibot.thv65.js
@@ -0,0 +1,17 @@
+// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+
+    // Write your Javascript code here using the Velo framework API
+
+    // Print hello world:
+    // console.log("Hello world!");
+
+    // Call functions on page elements, e.g.:
+    // $w("#button1").label = "Click me!";
+
+    // Click "Run", or Preview your site, to execute your code
+    <html>
+<!-- Accessibility Code for "chichibot.com" --> <script> window.interdeal = { "sitekey": "561a477a24fb96cbaaa0ce3be32e2e9c", "Position": "Right", "Menulang": "EN", "domains": { "js": "https://cdn.equalweb.com/", "acc": "https://access.equalweb.com/" }, "btnStyle": { "vPosition": [ "80%", null ], "scale": [ "0.8", "0.8" ], "color": { "main": "#e6051f" }, "icon": { "type": 8, "shape": "semicircle", "outline": false } } }; (function(doc, head, body){ var coreCall = doc.createElement('script'); coreCall.src = 'https://cdn.equalweb.com/core/4.4.0/accessibility.js'; coreCall.defer = true; coreCall.integrity = 'sha512-3lGJBcuai1J0rGJHJj4e4lYOzm7K08oEHsg1Llt7x24OOsa/Ca0wwbSi9HhWUn92FKN3fylaq9xmIKVZnUsT3Q=='; coreCall.crossOrigin = 'anonymous'; coreCall.setAttribute('data-cfasync', true ); body? body.appendChild(coreCall) : head.appendChild(coreCall); })(document, document.head, document.body); </script>
+</html>
+});
